0c50161e1fc733a7a116590afd2014e2b1f6c652
Darren Henderson <darrenh@ccsj.edu>
2018-07-13 20:30:53 -0500

    Add a FileSystem controller and view.

---
+++ MyShop/Controllers/FileSystemController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyShop.Controllers
+{
+    public class FileSystemController : Controller
+    {
+        public IActionResult Index()    
+        {
+            var files = System.IO.Directory.GetFiles(@"c:\");
+            ViewBag.FilesList = files;
+
+            return View(files);
+        }
+    }
+}
