73905a8fef07aba0436ae41094682900a68e9c41
Darren Henderson <darrenh@ccsj.edu>
2018-07-14 10:16:35 -0500

    Create the Product model and DbContext.

---
+++ MyShop/Data/ShopDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyShop.Models;
+
+
+namespace MyShop.Data
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        
+    }
+}
